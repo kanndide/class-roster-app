40cae0121ce7b420e72e036110805c1b230673ba
kanndide <kanndide@gmail.com>
2018-06-19 13:41:28 +0300

    create_student routes done, added courses_controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,19 @@
 class StudentsController < ApplicationContoller
 
 	get '/students/create_new' do
-		erb :'/students/create_student'
+		@courses = current_user.courses
+  		if @courses
+  			erb :'/students/create_student'
+  		else
+  			redirect '/login'
+  		end
+	end
+
+	post '/students/create_new' do
+		@student = Student.create(params[:student])
+  		@course = Course.find_by(params[:course])
+  		@student.student_courses.create(course: @course)
+  		@student.save
 	end
 
 end
