428666be9d9d4e58dd887ef453f316894a9dad98
kanndide <kanndide@gmail.com>
2018-06-19 13:44:05 +0300

    courses routes and controller added

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,14 @@
 class CoursesController < ApplicationController
 
+	get '/courses/create_new' do
+		erb :'/courses/create_course'
+	end
+
+	post '/courses/create_new' do
+		@course = Course.create(params)
+  		@course.teacher_id = current_user.id
+  		@course.save
+  		redirect '/courses/create_new'
+	end
+
 end
